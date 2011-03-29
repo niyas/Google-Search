267ae4b38274156e7070c19926bdf104be14aef1
niyas <niyas.n@innoppl.com>
2011-03-29 04:58:30 -0400

    search

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saravanan::Application.config.secret_token = 'ccae415d62e602cc20cc8c303342f3f56c95f46345f8b215c4a57b30b2eeb4548710e56057e989813d07ad6896ccf91e47b1715a860d6f53f4d8c693124e050c'
